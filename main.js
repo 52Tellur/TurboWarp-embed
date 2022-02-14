eca51a149dd775e2abdadaa1430bfa8fdecab113
52Tellur/テルル <81295209+52Tellur@users.noreply.github.com>
2022-02-14 17:39:04 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const showMessage = () => {
+    const textbox = document.getElementById("URL");
+    const inputValue = textbox.value;
+    const TBwidth = document.getElementById("width");//text box width
+    const inputWidth = TBwidth.value;
+    const TBheight = document.getElementById("height");
+    const inputHeight =TBheight.value;
+
+    const parameters = "";
+    //ここからオプション
+    /*
+    const autoplay=document.getElementById('autoplay');
+    if (autoplay.checked){
+        parameters = parameters + "\?autoplay";
+    }
+    */
+    //出力するメッセージを生成
+    let output = "\<iframe src=\"https://turbowarp.org/" + inputValue + "/embed" + parameters + "\" width=\"" + inputWidth + "\" height=\"" + inputHeight + "\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"no\" allowfullscreen\>\</iframe\>";
+
+    //合体！いったいどうなるんだー
+    //表示
+    embed.innerHTML = output;
+
+    //コード表示用に変換
+    code = output.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g,'&quot').replace(/\//g,'&#047;').replace(/=/g,'&#061;');
+    //出力
+    embedcode.innerHTML = code;
+  }
