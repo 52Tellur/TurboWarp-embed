27b7b331e5cb50e7dd226ecad419b1fb35e41412
52Tellur/テルル <81295209+52Tellur@users.noreply.github.com>
2022-05-20 20:37:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const showMessage = () => {
     let parameters = "?";
     
     //ここからオプション
-    let cloud_host = document.getElementById('cloud');
-        parameters = parameters + "cloud_host= " + cloud_host ;
+    let cloud_host = document.getElementById('cloud_host');
+        parameters = parameters + "cloud_host=" + cloud_host ;
     let autoplay=document.getElementById('autoplay');
     if (autoplay.checked){
         if (parameters == "?"){
